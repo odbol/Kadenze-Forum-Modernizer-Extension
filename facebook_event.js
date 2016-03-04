69fb38b11c63339f235fce2ed09c46c45ff3c6dc
odbol <git@odbol.com>
2016-03-03 22:22:21 -0800

    Made more efficient. Added nice cover up overlay

--- facebook_event.js
+++ facebook_event.js
@@ -2,13 +2,25 @@
 
 var FACEBOOK_DIV_TO_HIDE = '.userContentWrapper .userContent';
 
-function checkPosts() {
-	var crap = jQuery(FACEBOOK_DIV_TO_HIDE).next();
+var hasScrolled = 0,
+	checkPosts = function () {
+		
+		if (hasScrolled++ < 10) {
+			hasScrolled = 0;
 
+			var crap = jQuery(FACEBOOK_DIV_TO_HIDE).next();
 
-	crap.addClass('memefree_hidden');
-}
+			crap.addClass('memefree_hidden');
+
+		}
+	},
+	onScroll = function () {
+		hasScrolled = 0;
+	};
+
 
 
 // Since facebook uses the history API, I have to check for facebook event URLs
 setInterval(checkPosts, 1000);
+
+window.addEventListener('scroll', onScroll);
